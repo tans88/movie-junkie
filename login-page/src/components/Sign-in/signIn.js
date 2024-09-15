6534b25d6b630905f2ec19867dcade53ad01298a
Tanya Rath <emailtanya8@gmail.com>
2024-09-15 11:13:32 -0700

    added regex to sign up page

--- login-page/src/components/Sign-in/signIn.js
+++ login-page/src/components/Sign-in/signIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import arrow from './arrow.svg';
 import { GoogleLogin } from '@react-oauth/google';
 import './signIn.css';
@@ -6,14 +6,32 @@ import './signIn.css';
 const clientId = '114509371072-7tarhegmoe9enjf1k3venbm3h7uap4fa.apps.googleusercontent.com';
 
 function SignIn({ onSignUpClick }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
       <div className='login-component'>
   
         {/* Login Form */}
         <form>
           <h1>SIGN IN</h1>
-          <input className='email' type='email' name='email' placeholder='Email' required />
-          <input className='password' type='password' name='password' placeholder='Password' required />
+          <input 
+          className='email' 
+          type='email' 
+          name='email' 
+          placeholder='Email' 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required />
+          <input 
+          className='password' 
+          type='password' 
+          name='password' 
+          placeholder='Password' 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required />
+          
           <button type="submit" className="btn" id="btn">
             <img src={arrow} alt="arrow" className="arrow-icon" />
           </button>
--- login-page/src/components/Sign-up/signUp.js
+++ login-page/src/components/Sign-up/signUp.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signUp.css';
 import arrow from '../Sign-in/arrow.svg';
 
 function SignUp({ onSignInClick }) {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isConfirmed, setIsConfirmed] = useState(false);
 
     // checks if password meets criteria and if passwords match
-    const processPassword = () => {
+    const checkPassword = (password) => {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+        return passwordRegex.test(password);
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!checkPassword(password)){
+            setErrorMessage('Password must be 8 characters long, have at least one capital letter, at least one number, and at least one speical character.');
+            setIsConfirmed(false);
+            return;
+        }
+
+        if (password !== confirmPassword){
+            setErrorMessage('Passwords do not match.');
+            setIsConfirmed(false);
+            return;
+        }
+
+        setErrorMessage('');
+        setIsConfirmed(true);
     };
 
     return (
@@ -14,12 +38,39 @@ function SignUp({ onSignInClick }) {
 
         <form>
           <h1>CREATE ACCOUNT</h1>
-          <input className='email' type='email' name='email' placeholder='Email' required />
-          <input className='password' type='password' name='password' placeholder='Password' required />
-          <input className='confirm-password' type='password' name='password' placeholder='Confirm Password' required />
-          <button onClick={processPassword} type="submit" className="btn" id="btn">
+          <input 
+          className='email' 
+          type='email' 
+          name='email' 
+          placeholder='Email' 
+          required />
+          <input 
+          className='password' 
+          type='password' 
+          name='password' 
+          placeholder='Password' 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required />
+          <input 
+          className='confirm-password' 
+          type='password' 
+          name='password' 
+          placeholder='Confirm Password' 
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required />
+
+          <button onClick={handleSubmit} type="submit" className="btn" id="btn">
             <img src={arrow} alt="arrow" className="arrow-icon" />
           </button>
+
+          {/** Display error message */}
+          {isConfirmed ? (
+            <p className='message confirmation'>Form submitted!</p>
+          ) : (
+            <p className='message error-message'>{errorMessage}</p>
+          )}
         </form>
   
         <p className='sign-in'>Already a member? <span onClick={onSignInClick}>Sign in now</span></p>
