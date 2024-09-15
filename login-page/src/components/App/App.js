002aad6d2d3ecbbbd2d30f2bbbab71f6dfb960e2
Tanya Rath <emailtanya8@gmail.com>
2024-09-14 20:55:25 -0700

    changed files around and finished sign in page

---
+++ login-page/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import SignIn from '../Sign-in/signIn';
+
+function App() {
+  return (
+    <div className="login-page">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
